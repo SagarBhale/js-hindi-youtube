f25a398e74c1f15326b3829dc91f63ff538fb388
SagarBhale <94820935+SagarBhale@users.noreply.github.com>
2023-10-01 07:11:57 +0000

    datatypers

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3)  //we are using node.js, not browser
+
+console.log(3
+    +
+    3) // code readability should be high 
+
+console.log("Hitesh")
+
+let name = "Hiesh" //string
+let age = 18 //numer
+let isLoggedIn = false //boolean
+
+
+//--------PRIMITVE DATA TYPES----------//
+// number ==>  2 to power 53
+// bigint ==> 
+// string ==> ""
+// boolean => true/false
+// null   ==> standalone value(null is representaion of emplty type) It is an object
+// undefined ==> value not defined
+// symbol ==> unique
+
+
+//----------OBJECT----------------------//
+
+console.log(typeof "Hitesh"); // sting 
+console.log(typeof null);   //object 
+console.log(typeof undefined); //undefined
+
+
+
+
+
+ 
+
+
