e076699b5fa24b69c4be32fccce7cf11ce9c7ea6
SagarBhale <94820935+SagarBhale@users.noreply.github.com>
2023-10-01 10:27:44 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "Sagar "
+const repoCount = 50
+
+console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Hitesh --SHHH")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.charAt(0));
+// console.log(gameName.charAt(3));
+
+console.log(gameName.charAt(5));
+
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(4,6)
+console.log(newString);
+
+
