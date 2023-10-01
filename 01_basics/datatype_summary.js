f7d8ef78f6f4d087a9a847969a2db11a5ea55be6
SagarBhale <94820935+SagarBhale@users.noreply.github.com>
2023-10-01 10:05:04 +0000

    datatype_summary

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -43,4 +43,39 @@ console.log(typeof heros);
 
 console.log(typeof anotherId);
 
-//
+
+
+//==========================+++++++++++++++++++===============
+//  MEMORY
+// 1. Stack(Primitive)(mtlb jab bhi variable declare hote he to copy milti ), 
+//2.Heap(Non-Primitive)( jab bhi array vgera declare hota he to reference milta he orginal value ka)
+
+let myYoutubename = "sagarbhale.com"  //stack(Primitive)
+
+let anothername = "chaiorcode"
+
+console.log(myYoutubename)
+console.log(anothername);
+
+let userOne = {                   //copy nhi original value ka reference milta 
+    email: "useratgoogle.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email="Hitesh@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
+//Means STACK me kisi bhi variable ki value ko change kre 
+// to original value me koi change nhi hota he 
+
+//HEAP me original value me bhi change hoga agr hum usko reference ke through update kre to 
+
+
+
+
+
+
+
