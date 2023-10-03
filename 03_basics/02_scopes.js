a57e6fede7f86d45d0900a6edd972f5e19f8bd65
SagarBhale <94820935+SagarBhale@users.noreply.github.com>
2023-10-03 16:35:19 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -18,11 +18,11 @@ if(true){
 //console.log("global-c=",c);
 
 //in this for loop it is local block
-for (let i = 0; i < array.length; i++) {
+/*for (let i = 0; i < array.length; i++) {
     const element = array[i];
     
 }
-
+*/
 // Interview => what is global scope in node and windows(console),what is clouser
 //
 //--------------------------------------------------------------------------------------
@@ -31,11 +31,34 @@ function one(){
     const username = "Hitesh";
     
     function two(){
-        cosnt website ="Youtube"
-        console.log(username);
+        const website ="Youtube"
+       // console.log(username);
     }
-    console.log(website);
+    //console.log(website);
+
     two();
 }
 one();
 
+if(true){
+    const username = "Sagar";
+    if(username==="Sagar")
+    {
+        const website =" Youtube";
+        console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+//+++++++++++++++++++++ INTERESTING CONCEPT ++++++++++++++++++++++++++++++
+console.log(addone(5));
+function addone(num){
+    return num + 1;
+}
+
+
+addTwo(5); //give error -> hoiting
+const addTwo = function(num){
+    return num + 2;
+}
